a5c4d309d74616f2749918f3fbc1991087fb1d77
Connor Flowers <cjflowers@pm.me>
2025-09-01 14:01:58 +1000

    generate city instead of single block

--- src/world/concrete.ts
+++ src/world/concrete.ts
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 
-const NUM_CONCRETE_TOWERS = 20;
+const NUM_CONCRETE_TOWERS = 200;
+const CITY_WIDTH = 50;
+const CITY_DEPTH = 50;
 
 export class Concrete {
   private material: THREE.MeshStandardMaterial;
@@ -16,17 +18,10 @@ export class Concrete {
   }
 
   generate() {
-    const base = new THREE.Mesh(
-      new THREE.BoxGeometry(15, 1, 15),
-      this.material,
-    );
-    base.receiveShadow = true;
-    this.group.add(base);
-
     for (let i = 0; i < NUM_CONCRETE_TOWERS; i++) {
-      const width = Math.random() * 2 + 1;
-      const height = Math.random() * 8 + 4;
-      const depth = Math.random() * 2 + 1;
+      const width = Math.random() * 3 + 1;
+      const height = Math.random() * 15 + 5;
+      const depth = Math.random() * 3 + 1;
 
       const tower = new THREE.Mesh(
         new THREE.BoxGeometry(width, height, depth),
@@ -36,9 +31,9 @@ export class Concrete {
       tower.receiveShadow = true;
 
       tower.position.set(
-        (Math.random() - 0.5) * 9,
-        height / 2 + 0.5,
-        (Math.random() - 0.5) * 9,
+        (Math.random() - 0.5) * CITY_WIDTH,
+        height / 2,
+        (Math.random() - 0.5) * CITY_DEPTH,
       );
 
       this.group.add(tower);
